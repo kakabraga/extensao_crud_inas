f508d52588d7cf484a1a917104d231f05f4353e1
kakabraga <167010103+kakabraga@users.noreply.github.com>
2025-10-08 21:03:08 -0300

    Teste new branch

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
        fetchHours();
     });
 
-	//fetchHours();
+	//fetchHours(); chamada da função
 
 	context.subscriptions.push(horario);
 }
